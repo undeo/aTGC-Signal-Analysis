92e4bf38c0ab16ee76bcc543b9c02fce337045e9
Christoph Renner <undeo@student.kit.edu>
2016-02-23 17:29:22 +0100

    optimized for any parameter

--- ATGCRooStatsTMP/make_PDF_input.py
+++ ATGCRooStatsTMP/make_PDF_input.py
@@ -61,7 +61,7 @@ def make_SMBKG_input(ch = "ele", POIs = ["cwww"], oldtrees = 0):
 				c_wwwl[0] = -12; c_wl[0] = 0; c_bl[0] = 0;
 				weight[0] = treeInATGC.aTGCWeights[1] * weight_part
 				treeATGC.Fill()
-			if "cw" in POIs:
+			if "ccw" in POIs:
 				c_wwwl[0] = 0; c_wl[0] = 20; c_bl[0] = 0;
 				weight[0] = treeInATGC.aTGCWeights[2] * weight_part
 				treeATGC.Fill()
@@ -80,9 +80,9 @@ def make_SMBKG_input(ch = "ele", POIs = ["cwww"], oldtrees = 0):
 
 
 	#prepare fit
-	a1		= RooRealVar("a1_%s"%ch,"a1_%s"%ch,-0.1,-2,0)	
+	a1		= RooRealVar("a_SM_%s"%ch,"a_SM_%s"%ch,-0.1,-2,0)	
 	cwww		= RooRealVar("cwww","cwww",0,-12,12); 		cwww.setConstant(kTRUE);
-	cw		= RooRealVar("cw","cw",0,-20,20);		cw.setConstant(kTRUE);
+	ccw		= RooRealVar("ccw","ccw",0,-20,20);		ccw.setConstant(kTRUE);
 	cb		= RooRealVar("cb","cb",0,-60,60);		cb.setConstant(kTRUE);
 	#make and fill SM histogram, SM fit
 	SMhist		= TH1F("SMhist","SMhist",nbins4fit,binlo,binhi)
@@ -96,12 +96,11 @@ def make_SMBKG_input(ch = "ele", POIs = ["cwww"], oldtrees = 0):
 	#make and fill ATGC histograms
 	N1_factors	= []
 	exp_factors	= []
-	par_max		= []
+	par_max		= [12,20,60]
 	PDFs		= []
 	hists		= [SMdatahist]
 	for para in POIs:
 		if para == "cwww":
-			par_max.append(12)
 			hist4fit_cwww	= TH1F("hist4fit_cwww","hist4fit_cwww",3,-1.5*par_max[0],1.5*par_max[0])
 			cwww_pos_hist	= TH1F("c_pos_hist_cwww","c_pos_hist_cwww",nbins4fit,binlo,binhi)
 			cwww_neg_hist	= TH1F("c_neg_hist_cwww","c_neg_hist_cwww",nbins4fit,binlo,binhi)
@@ -118,47 +117,47 @@ def make_SMBKG_input(ch = "ele", POIs = ["cwww"], oldtrees = 0):
 			hist4fit_cwww.SetBinContent(3,cwww_pos_datahist.sumEntries()/SMdatahist.sumEntries())
 			hist4fit_cwww.Fit("pol2")
 			fitfunc_cwww		= hist4fit_cwww.GetFunction("pol2")
-			par0_cwww		= RooRealVar("par0_cwww","par0_cwww",fitfunc_cwww.GetParameter(0)); 		par0_cwww.setConstant(kTRUE);
-			par1_cwww		= RooRealVar("par1_cwww","par1_cwww",fitfunc_cwww.GetParameter(1)); 		par1_cwww.setConstant(kTRUE);
-			par2_cwww		= RooRealVar("par2_cwww","par2_cwww",fitfunc_cwww.GetParameter(2)); 		par2_cwww.setConstant(kTRUE);
+			par0_cwww		= RooRealVar("par0_cwww_%s"%ch,"par0_cwww_%s"%ch,fitfunc_cwww.GetParameter(0)); 		par0_cwww.setConstant(kTRUE);
+			par1_cwww		= RooRealVar("par1_cwww_%s"%ch,"par1_cwww_%s"%ch,fitfunc_cwww.GetParameter(1)); 		par1_cwww.setConstant(kTRUE);
+			par2_cwww		= RooRealVar("par2_cwww_%s"%ch,"par2_cwww_%s"%ch,fitfunc_cwww.GetParameter(2)); 		par2_cwww.setConstant(kTRUE);
 			scaleshape_cwww		= RooFormulaVar("scaleshape_cwww","scaleshape_cwww","@0+@1*@3+@2*@3*@3",RooArgList(par0_cwww,par1_cwww,par2_cwww,cwww))
 			scaleshapemax_cwww	= RooRealVar("scaleshapemax_cwww","scaleshapemax_cwww",hist4fit_cwww.GetMaximum());		scaleshapemax_cwww.setConstant(kTRUE);
 			N1_cwww			= RooFormulaVar("N1_cwww","N1_cwww","abs((@0-1)/(@1-1))",RooArgList(scaleshape_cwww,scaleshapemax_cwww))
 			N1_factors.append(N1_cwww)
-			a2		= RooRealVar("a_%s_%s"%(para,ch),"a2_%s_%s"%(para,ch),-0.001,-2,0)
+			a2		= RooRealVar("a_%s_%s"%(para,ch),"a_%s_%s"%(para,ch),-0.001,-2,0)
 			cwwwPdf		= RooExponential("cwwwPdf","cwwwPdf",rrv_mass_lvj,a2)
 			PDFs.append(cwwwPdf)
 			hists.extend([cwww_neg_datahist,cwww_pos_datahist])
 			exp_factors.append(a2)
-		if para == "cw":
+		if para == "ccw":
 			par_max.append(20)
-			hist4fit_cw	= TH1F("hist4fit_cw","hist4fit_cw",3,-1.5*par_max[1],1.5*par_max[1])
-			cw_pos_hist	= TH1F("c_pos_hist_cw","c_pos_hist_cw",nbins4fit,binlo,binhi)
-			cw_neg_hist	= TH1F("c_neg_hist_cw","c_neg_hist_cw",nbins4fit,binlo,binhi)
+			hist4fit_ccw	= TH1F("hist4fit_ccw","hist4fit_ccw",3,-1.5*par_max[1],1.5*par_max[1])
+			ccw_pos_hist	= TH1F("c_pos_hist_ccw","c_pos_hist_ccw",nbins4fit,binlo,binhi)
+			ccw_neg_hist	= TH1F("c_neg_hist_ccw","c_neg_hist_ccw",nbins4fit,binlo,binhi)
 			for i in range(treeATGC.GetEntries()):
 				treeATGC.GetEntry(i)
-			    	if (para == "cw" and treeATGC.c_wl == par_max[1]):
-			      		cw_pos_hist.Fill(treeATGC.m_lvj,treeATGC.weight)
-			    	if (para == "cw" and treeATGC.c_wl == -par_max[1]):
-			      		cw_neg_hist.Fill(treeATGC.m_lvj,treeATGC.weight)
-			cw_pos_datahist	= RooDataHist("cw_pos_datahist","cw_pos_datahist",RooArgList(rrv_mass_lvj),cw_pos_hist)
-			cw_neg_datahist	= RooDataHist("cw_neg_datahist","cw_neg_datahist",RooArgList(rrv_mass_lvj),cw_neg_hist)
-			hist4fit_cw.SetBinContent(1,cw_neg_datahist.sumEntries()/SMdatahist.sumEntries())
-			hist4fit_cw.SetBinContent(2,1)
-			hist4fit_cw.SetBinContent(3,cw_pos_datahist.sumEntries()/SMdatahist.sumEntries())
-			hist4fit_cw.Fit("pol2")
-			fitfunc_cw		= hist4fit_cw.GetFunction("pol2")
-			par0_cw			= RooRealVar("par0_cw","par0_cw",fitfunc_cw.GetParameter(0)); 		par0_cw.setConstant(kTRUE);
-			par1_cw			= RooRealVar("par1_cw","par1_cw",fitfunc_cw.GetParameter(1)); 		par1_cw.setConstant(kTRUE);
-			par2_cw			= RooRealVar("par2_cw","par2_cw",fitfunc_cw.GetParameter(2)); 		par2_cw.setConstant(kTRUE);
-			scaleshape_cw		= RooFormulaVar("scaleshape_cw","scaleshape_cw","@0+@1*@3+@2*@3*@3",RooArgList(par0_cw,par1_cw,par2_cw,cw))
-			scaleshapemax_cw	= RooRealVar("scaleshapemax_cw","scaleshapemax_cw",hist4fit_cw.GetMaximum());		scaleshapemax_cw.setConstant(kTRUE);
-			N1_cw			= RooFormulaVar("N1_cw","N1_cw","abs((@0-1)/(@1-1))",RooArgList(scaleshape_cw,scaleshapemax_cw))
-			N1_factors.append(N1_cw)
-			a3		= RooRealVar("a_%s_%s"%(para,ch),"a3_%s_%s"%(para,ch),-0.001,-2,0)
-			cwPdf		= RooExponential("cwPdf","cwPdf",rrv_mass_lvj,a3)
-			PDFs.append(cwPdf)
-			hists.extend([cw_neg_datahist,cw_pos_datahist])
+			    	if (para == "ccw" and treeATGC.c_wl == par_max[1]):
+			      		ccw_pos_hist.Fill(treeATGC.m_lvj,treeATGC.weight)
+			    	if (para == "ccw" and treeATGC.c_wl == -par_max[1]):
+			      		ccw_neg_hist.Fill(treeATGC.m_lvj,treeATGC.weight)
+			ccw_pos_datahist	= RooDataHist("ccw_pos_datahist","ccw_pos_datahist",RooArgList(rrv_mass_lvj),ccw_pos_hist)
+			ccw_neg_datahist	= RooDataHist("ccw_neg_datahist","ccw_neg_datahist",RooArgList(rrv_mass_lvj),ccw_neg_hist)
+			hist4fit_ccw.SetBinContent(1,ccw_neg_datahist.sumEntries()/SMdatahist.sumEntries())
+			hist4fit_ccw.SetBinContent(2,1)
+			hist4fit_ccw.SetBinContent(3,ccw_pos_datahist.sumEntries()/SMdatahist.sumEntries())
+			hist4fit_ccw.Fit("pol2")
+			fitfunc_ccw		= hist4fit_ccw.GetFunction("pol2")
+			par0_ccw		= RooRealVar("par0_ccw_%s"%ch,"par0_ccw_%s"%ch,fitfunc_ccw.GetParameter(0)); 		par0_ccw.setConstant(kTRUE);
+			par1_ccw		= RooRealVar("par1_ccw_%s"%ch,"par1_ccw_%s"%ch,fitfunc_ccw.GetParameter(1)); 		par1_ccw.setConstant(kTRUE);
+			par2_ccw		= RooRealVar("par2_ccw_%s"%ch,"par2_ccw_%s"%ch,fitfunc_ccw.GetParameter(2)); 		par2_ccw.setConstant(kTRUE);
+			scaleshape_ccw		= RooFormulaVar("scaleshape_ccw","scaleshape_ccw","@0+@1*@3+@2*@3*@3",RooArgList(par0_ccw,par1_ccw,par2_ccw,ccw))
+			scaleshapemax_ccw	= RooRealVar("scaleshapemax_ccw","scaleshapemax_ccw",hist4fit_ccw.GetMaximum());		scaleshapemax_ccw.setConstant(kTRUE);
+			N1_ccw			= RooFormulaVar("N1_ccw","N1_ccw","abs((@0-1)/(@1-1))",RooArgList(scaleshape_ccw,scaleshapemax_ccw))
+			N1_factors.append(N1_ccw)
+			a3		= RooRealVar("a_%s_%s"%(para,ch),"a_%s_%s"%(para,ch),-0.001,-2,0)
+			ccwPdf		= RooExponential("ccwPdf","ccwPdf",rrv_mass_lvj,a3)
+			PDFs.append(ccwPdf)
+			hists.extend([ccw_neg_datahist,ccw_pos_datahist])
 			exp_factors.append(a3)
 		if para == "cb":
 			par_max.append(60)
@@ -178,14 +177,14 @@ def make_SMBKG_input(ch = "ele", POIs = ["cwww"], oldtrees = 0):
 			hist4fit_cb.SetBinContent(3,cb_pos_datahist.sumEntries()/SMdatahist.sumEntries())
 			hist4fit_cb.Fit("pol2")
 			fitfunc_cb		= hist4fit_cb.GetFunction("pol2")
-			par0_cb			= RooRealVar("par0_cb","par0_cb",fitfunc_cb.GetParameter(0)); 		par0_cb.setConstant(kTRUE);
-			par1_cb			= RooRealVar("par1_cb","par1_cb",fitfunc_cb.GetParameter(1)); 		par1_cb.setConstant(kTRUE);
-			par2_cb			= RooRealVar("par2_cb","par2_cb",fitfunc_cb.GetParameter(2)); 		par2_cb.setConstant(kTRUE);
+			par0_cb			= RooRealVar("par0_cb_%s"%ch,"par0_cb_%s"%ch,fitfunc_cb.GetParameter(0)); 		par0_cb.setConstant(kTRUE);
+			par1_cb			= RooRealVar("par1_cb_%s"%ch,"par1_cb_%s"%ch,fitfunc_cb.GetParameter(1)); 		par1_cb.setConstant(kTRUE);
+			par2_cb			= RooRealVar("par2_cb_%s"%ch,"par2_cb_%s"%ch,fitfunc_cb.GetParameter(2)); 		par2_cb.setConstant(kTRUE);
 			scaleshape_cb		= RooFormulaVar("scaleshape_cb","scaleshape_cb","@0+@1*@3+@2*@3*@3",RooArgList(par0_cb,par1_cb,par2_cb,cb))
 			scaleshapemax_cb	= RooRealVar("scaleshapemax_cb","scaleshapemax_cb",hist4fit_cb.GetMaximum());		scaleshapemax_cb.setConstant(kTRUE);
 			N1_cb			= RooFormulaVar("N1_cb","N1_cb","abs((@0-1)/(@1-1))",RooArgList(scaleshape_cb,scaleshapemax_cb))
 			N1_factors.append(N1_cb)
-			a4		= RooRealVar("a_%s_%s"%(para,ch),"a4_%s_%s"%(para,ch),-0.001,-2,0)
+			a4		= RooRealVar("a_%s_%s"%(para,ch),"a_%s_%s"%(para,ch),-0.001,-2,0)
 			cbPdf		= RooExponential("cbPdf","cbPdf",rrv_mass_lvj,a4)
 			PDFs.append(cbPdf)
 			hists.extend([cb_neg_datahist,cb_pos_datahist])
@@ -202,7 +201,7 @@ def make_SMBKG_input(ch = "ele", POIs = ["cwww"], oldtrees = 0):
 			can2		= TCanvas("canvas2",POIs[1],1)
 			p1 		= rrv_mass_lvj.frame()
 			p2 		= rrv_mass_lvj.frame()
-		model		= RooAddPdf("aTGC-model","aTGC-model",RooArgList(PDFs[0],PDFs[1],SMPdf),RooArgList(N1_factors[0],N1_factors[1]))
+		model		= RooAddPdf("aTGC_model_%s"%ch,"aTGC_model_%s"%ch,RooArgList(PDFs[0],PDFs[1],SMPdf),RooArgList(N1_factors[0],N1_factors[1]))
 		POI_list	= [N1_factors[0].getParameter("scaleshape_%s"%POIs[0]).getParameter("%s"%POIs[0]),N1_factors[1].getParameter("scaleshape_%s"%POIs[1]).getParameter("%s"%POIs[1])]
 		#fit first pdf
 		POI_list[0].setVal(par_max[0])
@@ -217,7 +216,8 @@ def make_SMBKG_input(ch = "ele", POIs = ["cwww"], oldtrees = 0):
 		POI_list[1].setVal(par_max[1])
 		POI_list[0].setVal(0)
 		fitres2		= model.fitTo(hists[4],RooFit.Save(kTRUE), RooFit.SumW2Error(kTRUE));	exp_factors[1].setConstant(kTRUE);
-		fitres1.Print();	fitres2.Print();
+		fitres1.Print();	
+		fitres2.Print();
 		if do_plot:
 			hists[4].plotOn(p2,RooFit.MarkerColor(par_max[1]+1))
 			model.plotOn(p2,RooFit.LineColor(par_max[1]+1))
@@ -231,35 +231,42 @@ def make_SMBKG_input(ch = "ele", POIs = ["cwww"], oldtrees = 0):
 				POI_list[0].setVal(i);		
 				POI_list[1].setVal(0)
 				normval0	= SMdatahist.sumEntries()*N1_factors[0].getParameter("scaleshape_%s"%POIs[0]).getVal()
-				model.plotOn(p1,RooFit.LineWidth(1),RooFit.LineColor(i+1),RooFit.Normalization(normval0,RooAbsReal.NumEvent))
+				model.plotOn(p1,RooFit.LineWidth(2),RooFit.LineColor(i+1),RooFit.Normalization(normval0,RooAbsReal.NumEvent))
+				hists[0].plotOn(p1,RooFit.MarkerColor(kBlack))
+				hists[2].plotOn(p1,RooFit.MarkerColor(par_max[0]+1))
 			for i in range(par_max[1]):
 				POI_list[1].setVal(i);		
 				POI_list[0].setVal(0)
 				normval1	= SMdatahist.sumEntries()*N1_factors[1].getParameter("scaleshape_%s"%POIs[1]).getVal()
-				model.plotOn(p2,RooFit.LineWidth(1),RooFit.LineColor(i+1),RooFit.Normalization(normval1,RooAbsReal.NumEvent))
+				model.plotOn(p2,RooFit.LineWidth(2),RooFit.LineColor(i+1),RooFit.Normalization(normval1,RooAbsReal.NumEvent))
+				hists[0].plotOn(p2,RooFit.MarkerColor(kBlack))
+				hists[4].plotOn(p2,RooFit.MarkerColor(par_max[1]+1))
 
 			#draw plot
 			can1.cd()
-			p1.GetYaxis().SetRangeUser(0.03,50)
+			p1.GetYaxis().SetRangeUser(0.03,75)
 			p1.Draw()
+			can1.SetLogy()
 			can1.Update()
 			can2.cd()
-			p2.GetYaxis().SetRangeUser(0.03,50)
+			p2.GetYaxis().SetRangeUser(0.03,75)
 			p2.Draw()
+			can2.SetLogy()
 			can2.Update()
+			raw_input("...")
 
-
-		raw_input("@")
 	if len(PDFs) == 3:
-		raise RuntimeError(str(len(PDFs)) + " parameters not done yet!")
+		raise RuntimeError("3 parameters not done yet!")
 
 
 	#import to ws
-	getattr(w,"import")(model) 
+	getattr(w,"import")(model)
 
 	path	="../../CombinedEWKAnalysis/CommonTools/data/anomalousCoupling"
-	cwww 	= "_cwww" if "cwww" in POIs else ""; cw = "_cw" if "cw" in POIs else ""; cb = "_cb" if "cb" in POIs else "";
-	output 	= TFile("%s/ch_%s%s%s%s.root"%(path,ch,cwww,cw,cb),"recreate")
+	cwww 	= "_cwww" if "cwww" in POIs else ""
+	ccw = "_ccw" if "ccw" in POIs else "" 
+	cb = "_cb" if "cb" in POIs else ""
+	output 	= TFile("%s/ch_%s%s%s%s.root"%(path,ch,cwww,ccw,cb),"recreate")
 
 	data_obs.Write()
 	w.SetName("workspace"); w.Print(); w.Write();
@@ -267,5 +274,5 @@ def make_SMBKG_input(ch = "ele", POIs = ["cwww"], oldtrees = 0):
 	print "Write to file " + output.GetName()
 
   
-make_SMBKG_input("ele",["cwww","cw"],1)
-#make_SMBKG_input("mu",["cwww","cw"],0) 
+make_SMBKG_input("ele",["ccw"],0)
+make_SMBKG_input("mu",["ccw"],0)
