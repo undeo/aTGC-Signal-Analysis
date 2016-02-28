07f5ec6f5365d9fd546111fec1cb6183b3dbc43b
Christoph Renner <undeo@student.kit.edu>
2016-02-28 20:46:01 +0100

    made it even more readable

--- ATGCRooStatsTMP/make_PDF_input.py
+++ ATGCRooStatsTMP/make_PDF_input.py
@@ -44,31 +44,31 @@ def make_ATGCtree(fileInATGC='', ch='ele'):
 		c_bl[0]		= 0
 		weight[0]	= treeInATGC.aTGCWeights[7] * weight_part
 		treeATGC_tmp.Fill()
-		if 'cwww' in POI:
-			c_wwwl[0] = 12; c_wl[0]	= 0; c_bl[0] = 0;
-			weight[0] = treeInATGC.aTGCWeights[0] * weight_part
-			treeATGC_tmp.Fill()
-			c_wwwl[0] = -12; c_wl[0] = 0; c_bl[0] = 0;
-			weight[0] = treeInATGC.aTGCWeights[1] * weight_part
-			treeATGC_tmp.Fill()
-		if 'ccw' in POI:
-			c_wwwl[0] = 0; c_wl[0] = 20; c_bl[0] = 0;
-			weight[0] = treeInATGC.aTGCWeights[2] * weight_part
-			treeATGC_tmp.Fill()
-			c_wwwl[0] = 0; c_wl[0] = -20; c_bl[0] = 0;
-			weight[0] = treeInATGC.aTGCWeights[3] * weight_part
-			treeATGC_tmp.Fill()
-		if 'cb' in POI:
-			c_wwwl[0] = 0; c_wl[0] = 0; c_bl[0] = 60;
-			weight[0] = treeInATGC.aTGCWeights[4] * weight_part
-			treeATGC_tmp.Fill()
-			c_wwwl[0] = 0; c_wl[0] = 0; c_bl[0] = -60;
-			weight[0] = treeInATGC.aTGCWeights[5] * weight_part
-			treeATGC_tmp.Fill()
-		if 'cwww' in POI and 'ccw' in POI and 'cb' in POI:
-			c_wwwl[0] = 12; c_wl[0] = 20; c_bl[0] = 60;
-			weight[0] = treeInATGC.aTGCWeights[6] * weight_part
-			treeATGC_tmp.Fill()
+		#cwww
+		c_wwwl[0] = 12; c_wl[0]	= 0; c_bl[0] = 0;
+		weight[0] = treeInATGC.aTGCWeights[0] * weight_part
+		treeATGC_tmp.Fill()
+		c_wwwl[0] = -12; c_wl[0] = 0; c_bl[0] = 0;
+		weight[0] = treeInATGC.aTGCWeights[1] * weight_part
+		treeATGC_tmp.Fill()
+		#ccw
+		c_wwwl[0] = 0; c_wl[0] = 20; c_bl[0] = 0;
+		weight[0] = treeInATGC.aTGCWeights[2] * weight_part
+		treeATGC_tmp.Fill()
+		c_wwwl[0] = 0; c_wl[0] = -20; c_bl[0] = 0;
+		weight[0] = treeInATGC.aTGCWeights[3] * weight_part
+		treeATGC_tmp.Fill()
+		#cb
+		c_wwwl[0] = 0; c_wl[0] = 0; c_bl[0] = 60;
+		weight[0] = treeInATGC.aTGCWeights[4] * weight_part
+		treeATGC_tmp.Fill()
+		c_wwwl[0] = 0; c_wl[0] = 0; c_bl[0] = -60;
+		weight[0] = treeInATGC.aTGCWeights[5] * weight_part
+		treeATGC_tmp.Fill()
+		#all3
+		c_wwwl[0] = 12; c_wl[0] = 20; c_bl[0] = 60;
+		weight[0] = treeInATGC.aTGCWeights[6] * weight_part
+		treeATGC_tmp.Fill()
 		
 	treeATGC_tmp.Write()
 	treeATGC_tmp.Print()
@@ -76,7 +76,7 @@ def make_ATGCtree(fileInATGC='', ch='ele'):
 	
 	
 
-def make_plots(rrv_x,wtmp):
+def make_plots(rrv_x,wtmp,ch):
         
         can             = []
         plots	        = []
@@ -91,17 +91,24 @@ def make_plots(rrv_x,wtmp):
 			wtmp.var(POI[j]).setVal(0)
 		wtmp.data('SMdatahist').plotOn(plots[i],RooFit.MarkerColor(kBlue))
 		wtmp.data('pos_datahist_%s'%POI[i]).plotOn(plots[i],RooFit.MarkerColor(kRed))
-		normvalSM	= wtmp.function('normfactor_%sd'%len(POI)).getVal() * wtmp.data('SMdatahist').sumEntries()
-		wtmp.pdf('aTGC_model').plotOn(plots[i],RooFit.LineColor(kBlue),RooFit.Normalization(normvalSM, RooAbsReal.NumEvent))
+		normvalSM	= wtmp.function('normfactor_%sd_%s'%(len(POI),ch)).getVal() * wtmp.data('SMdatahist').sumEntries()
+		wtmp.pdf('aTGC_model').plotOn(plots[i],\
+					      RooFit.LineColor(kBlue),\
+					      RooFit.Normalization(normvalSM, RooAbsReal.NumEvent))
 		wtmp.var(POI[i]).setVal(par_max[POI[i]])
-		normval		= wtmp.function('normfactor_%sd'%len(POI)).getVal() * wtmp.data('SMdatahist').sumEntries()
-        	wtmp.pdf('aTGC_model').plotOn(plots[i],RooFit.LineColor(kRed),RooFit.Normalization(normval, RooAbsReal.NumEvent))
+		normval		= wtmp.function('normfactor_%sd_%s'%(len(POI),ch)).getVal() * wtmp.data('SMdatahist').sumEntries()
+        	wtmp.pdf('aTGC_model').plotOn(plots[i],\
+					      RooFit.LineColor(kRed),\
+					      RooFit.Normalization(normval, RooAbsReal.NumEvent))
+		for j in range(len(POI)):
+			wtmp.var(POI[j]).setVal(0)
 		for j in range(14):
-			for k in range(len(POI)):
-				wtmp.var(POI[k]).setVal(0)
 			wtmp.var(POI[i]).setVal(par_max[POI[i]]/15. * (j+1))
-			normval	= wtmp.function('normfactor_%sd'%len(POI)).getVal() * wtmp.data('SMdatahist').sumEntries()
-			wtmp.pdf('aTGC_model').plotOn(plots[i],RooFit.LineColor(kGreen),RooFit.LineStyle(kDashed),RooFit.Normalization(normval,RooAbsReal.NumEvent))
+			normval	= wtmp.function('normfactor_%sd_%s'%(len(POI),ch)).getVal() * wtmp.data('SMdatahist').sumEntries()
+			wtmp.pdf('aTGC_model').plotOn(plots[i],\
+						      RooFit.LineColor(kGreen),\
+						      RooFit.LineStyle(kDashed),\
+						      RooFit.Normalization(normval,RooAbsReal.NumEvent))
 		wtmp.data('SMdatahist').plotOn(plots[i],RooFit.MarkerColor(kBlue))
 		wtmp.data('pos_datahist_%s'%POI[i]).plotOn(plots[i],RooFit.MarkerColor(kRed))
 		plots[i].GetYaxis().SetRangeUser(0.03,100)
@@ -143,11 +150,15 @@ def make_input(ch = 'ele'):
 	treeATGC	= fileInATGC.Get('BasicTree')
 	treeATGC.Print()
 
-	#prepare fit
+	#prepare variables and parameters and temporary workspace
+	wtmp		= RooWorkspace('wtmp')
 	a1		= RooRealVar('a_SM_%s'%ch,'a_SM_%s'%ch,-0.1,-2,0)	
-	cwww		= RooRealVar('cwww','cwww',0,-12,12); 		cwww.setConstant(kTRUE);
-	ccw		= RooRealVar('ccw','ccw',0,-20,20);		ccw.setConstant(kTRUE);
-	cb		= RooRealVar('cb','cb',0,-60,60);		cb.setConstant(kTRUE);
+	cwww		= RooRealVar('cwww','cwww',0,-120,120); 	cwww.setConstant(kTRUE);	getattr(wtmp,'import')(cwww);
+	ccw		= RooRealVar('ccw','ccw',0,-200,200);		ccw.setConstant(kTRUE);		getattr(wtmp,'import')(ccw);
+	cb		= RooRealVar('cb','cb',0,-600,600);		cb.setConstant(kTRUE);		getattr(wtmp,'import')(cb);
+	cwww12		= RooFormulaVar('cwww12','cwww12','(@0/12)**2',RooArgList(cwww));		getattr(wtmp,'import')(cwww12);
+	ccw20		= RooFormulaVar('ccw20','ccw20','(@0/20)**2',RooArgList(ccw));			getattr(wtmp,'import')(ccw20);
+	cb60		= RooFormulaVar('cb60','cb60','(@0/60)**2',RooArgList(cb));			getattr(wtmp,'import')(cb60);
 
 	#make and fill SM histogram, SM fit
 	SMhist		= TH1F('SMhist','SMhist',nbins4fit,binlo,binhi)
@@ -160,7 +171,6 @@ def make_input(ch = 'ele'):
 	SMPdf.fitTo(SMdatahist, RooFit.SumW2Error(kTRUE));	a1.setConstant(kTRUE)
 
 	#make and fill ATGC histograms
-	wtmp		= RooWorkspace('wtmp')
 	getattr(wtmp,'import')(SMdatahist)
 
 	for para in POI:
@@ -187,54 +197,51 @@ def make_input(ch = 'ele'):
 		par0		= RooRealVar('par0_%s_%s'%(para,ch),'par0_%s_%s'%(para,ch),fitfunc.GetParameter(0)); 		par0.setConstant(kTRUE);
 		par1		= RooRealVar('par1_%s_%s'%(para,ch),'par1_%s_%s'%(para,ch),fitfunc.GetParameter(1)); 		par1.setConstant(kTRUE);
 		par2		= RooRealVar('par2_%s_%s'%(para,ch),'par2_%s_%s'%(para,ch),fitfunc.GetParameter(2)); 		par2.setConstant(kTRUE);
-		N1_ratio	= RooRealVar('N1_ratio_%s'%para,'N1_ratio_%s'%para,pos_datahist.sumEntries()/SMdatahist.sumEntries()-1)
-		if para == 'cwww':
-			N1		= RooFormulaVar('N1_%s'%para,'N1_%s'%para,'1/(1+@0*(@1/12)**2)',RooArgList(N1_ratio,cwww))
-			N2		= RooFormulaVar('N2_%s'%para,'N2_%s'%para,'(@0*(@1/12)**2)/(1+@0*(@1/12)**2)',RooArgList(N1_ratio,cwww))
-			scaleshape	= RooFormulaVar('scaleshape_%s'%para,'scaleshape%s'%para,'@0+@1*@3+@2*@3*@3',RooArgList(par0,par1,par2,cwww))
-		elif para == 'ccw':
-			N1		= RooFormulaVar('N1_%s'%para,'N1_%s'%para,'1/(1+@0*(@1/20)**2)',RooArgList(N1_ratio,ccw))
-			N2		= RooFormulaVar('N2_%s'%para,'N2_%s'%para,'(@0*(@1/20)**2)/(1+@0*(@1/20)**2)',RooArgList(N1_ratio,ccw))
-			scaleshape	= RooFormulaVar('scaleshape_%s'%para,'scaleshape%s'%para,'@0+@1*@3+@2*@3*@3',RooArgList(par0,par1,par2,ccw))
-		elif para == 'cb':
-			N1		= RooFormulaVar('N1_%s'%para,'N1_%s'%para,'1/(1+@0*(@1/60)**2)',RooArgList(N1_ratio,cb))
-			N2		= RooFormulaVar('N2_%s'%para,'N2_%s'%para,'(@0*(@1/60)**2)/(1+@0*(@1/60)**2)',RooArgList(N1_ratio,cb))
-			scaleshape	= RooFormulaVar('scaleshape_%s'%para,'scaleshape%s'%para,'@0+@1*@3+@2*@3*@3',RooArgList(par0,par1,par2,cb))			
+		N1_ratio	= RooRealVar('N1_ratio_%s'%para,'N1_ratio_%s'%para,pos_datahist.sumEntries()/SMdatahist.sumEntries()-1)		
+		scaleshape	= RooFormulaVar('scaleshape_%s_%s'%(para,ch),'scaleshape_%s_%s'%(para,ch),\
+						'@0+@1*@3+@2*@3*@3',\
+						RooArgList(par0,par1,par2,wtmp.var(para)))			
 		a2		= RooRealVar('a_%s_%s'%(para,ch),'a_%s_%s'%(para,ch),-0.001,-2,0)
 		a2.setConstant(kTRUE)
-		cwwwPdf		= RooExponential('Pdf_%s'%para,'Pdf_%s'%para,rrv_mass_lvj,a2)
-		getattr(wtmp,'import')(cwwwPdf)
+		cPdf		= RooExponential('Pdf_%s'%para,'Pdf_%s'%para,rrv_mass_lvj,a2)
+		getattr(wtmp,'import')(cPdf)
 		getattr(wtmp,'import')(scaleshape)
-		getattr(WS,'import')(scaleshape)
-		getattr(wtmp,'import')(N1);getattr(wtmp,'import')(N2);
-		getattr(wtmp,'import')(pos_datahist);getattr(wtmp,'import')(neg_datahist)
+		getattr(wtmp,'import')(N1_ratio)
+		getattr(wtmp,'import')(pos_datahist)
+		getattr(wtmp,'import')(neg_datahist)
 		wtmp.Print()
 
 	#make and fit model
 	if len(POI) == 1:
-		wtmp.var(POI[0]).setVal(par_max[POI[0]])
-		model 		= RooAddPdf('aTGC_model','aTGC_model',RooArgList(SMPdf,wtmp.pdf('Pdf_%s'%POI[0])),RooArgList(wtmp.function('N1_%s'%POI[0]),wtmp.function('N2_%s'%POI[0])))
-		wtmp.var('a_%s_%s'%(POI[0],ch)).setConstant(kFALSE)			
+		paralist	= RooArgList(N1_ratio,wtmp.function(POI[0]+str(par_max[POI[0]])))
+		N1_1d		= RooFormulaVar('N1_1d','N1_1d','1/(1+@0*@1)',paralist)
+		N2_1d		= RooFormulaVar('N2_1d','N2_1d','(@0*@1)/(1+@0*@1)',paralist)		
+		model 		= RooAddPdf('aTGC_model','aTGC_model',\
+					    RooArgList(SMPdf,wtmp.pdf('Pdf_%s'%POI[0])),\
+					    RooArgList(N1_1d,N2_1d))
+		wtmp.var('a_%s_%s'%(POI[0],ch)).setConstant(kFALSE)
+		wtmp.var(POI[0]).setVal(par_max[POI[0]])			
 		fitres		= model.fitTo(wtmp.data('pos_datahist_%s'%POI[0]),RooFit.Save(kTRUE), RooFit.SumW2Error(kTRUE))
 		wtmp.var('a_%s_%s'%(POI[0],ch)).setConstant(kTRUE)
 		fitres.Print()
-		normfactor_1d = RooFormulaVar('normfactor_1d','normfactor_1d','1+(@0-1)',RooArgList(wtmp.function('scaleshape_%s'%POI[0])))
+		normfactor_1d 	= RooFormulaVar('normfactor_1d_%s'%ch,'normfactor_1d_%s'%ch,\
+						'1+(@0-1)',\
+						RooArgList(wtmp.function('scaleshape_%s_%s'%(POI[0],ch))))
                 getattr(WS,'import')(normfactor_1d)
 		getattr(wtmp,'import')(normfactor_1d)
 		getattr(wtmp,'import')(model)
 		if options.do_plots:
-			make_plots(rrv_mass_lvj,wtmp)
+			make_plots(rrv_mass_lvj,wtmp,ch)
 
 	if len(POI) == 2:
-		if 'cwww' in POI:
-			cwww12	= RooFormulaVar('cwww12','cwww12','(@0/12)**2',RooArgList(wtmp.var('cwww')))
-		if 'ccw' in POI:
-			ccw20	= RooFormulaVar('ccw20','ccw20','(@0/20)**2',RooArgList(wtmp.var('ccw')))
-		paralist	= RooArgList(wtmp.function('N1_ratio_%s'%POI[0]),cwww12,wtmp.function('N1_ratio_%s'%POI[1]),ccw20)
+		paralist	= RooArgList(wtmp.function('N1_ratio_%s'%POI[0]),wtmp.function(POI[0]+str(par_max[POI[0]])),\
+						wtmp.function('N1_ratio_%s'%POI[1]),wtmp.function(POI[1]+str(par_max[POI[1]])))
 		N1_2d		= RooFormulaVar('N1_2d','N1_2d','1/(1+@0*@1+@2*@3)',paralist)
 		N2_2d		= RooFormulaVar('N2_2d','N2_2d','(@0*@1)/(1+@0*@1+@2*@3)',paralist)
 		N3_2d		= RooFormulaVar('N3_2d','N3_2d','(@2*@3)/(1+@0*@1+@2*@3)',paralist)		
-		model		= RooAddPdf('aTGC_model','aTGC_model',RooArgList(SMPdf,wtmp.pdf('Pdf_%s'%POI[0]),wtmp.pdf('Pdf_%s'%POI[1])),RooArgList(N1_2d,N2_2d,N3_2d))
+		model		= RooAddPdf('aTGC_model','aTGC_model',\
+					    RooArgList(SMPdf,wtmp.pdf('Pdf_%s'%POI[0]),wtmp.pdf('Pdf_%s'%POI[1])),\
+					    RooArgList(N1_2d,N2_2d,N3_2d))
 		model.Print()
 		#fit first pdf
 		wtmp.var(POI[0]).setVal(par_max[POI[0]])
@@ -250,27 +257,30 @@ def make_input(ch = 'ele'):
 		wtmp.var('a_%s_%s'%(POI[1],ch)).setConstant(kTRUE);
 		fitres1.Print()
 		fitres2.Print()
-		normfactor_2d = RooFormulaVar('normfactor_2d','normfactor_2d','1+((@0-1)+(@1-1))',RooArgList(wtmp.function('scaleshape_%s'%POI[0]),wtmp.function('scaleshape_%s'%POI[1])))
+		normfactor_2d = RooFormulaVar('normfactor_2d_%s'%ch,'normfactor_2d_%s'%ch,'1+((@0-1)+(@1-1))',\
+					      RooArgList(wtmp.function('scaleshape_%s_%s'%(POI[0],ch)),\
+							 wtmp.function('scaleshape_%s_%s'%(POI[1],ch))))
                 getattr(WS,'import')(normfactor_2d)
 		getattr(wtmp,'import')(normfactor_2d)
 		getattr(wtmp,'import')(model)
 		if options.do_plots:
-			make_plots(rrv_mass_lvj,wtmp)
+			make_plots(rrv_mass_lvj,wtmp,ch)
 
 	if len(POI) == 3:
-		if 'cwww' in POI:
-			cwww12	= RooFormulaVar('cwww12','cwww12','(@0/12)**2',RooArgList(wtmp.var('cwww')))
-		if 'ccw' in POI:
-			ccw20	= RooFormulaVar('ccw20','ccw20','(@0/20)**2',RooArgList(wtmp.var('ccw')))
-		if 'cb' in POI:
-			cb60	= RooFormulaVar('cb60','cb60','(@0/60)**2',RooArgList(wtmp.var('cb')))
-		paralist	= RooArgList(wtmp.function('N1_ratio_%s'%POI[0]),cwww12,wtmp.function('N1_ratio_%s'%POI[1]),ccw20,wtmp.function('N1_ratio_%s'%POI[2]),cb60)
+		paralist	= RooArgList(wtmp.function('N1_ratio_%s'%POI[0]),wtmp.function(POI[0]+str(par_max[POI[0]])),\
+					     wtmp.function('N1_ratio_%s'%POI[1]),wtmp.function(POI[1]+str(par_max[POI[1]])),\
+					     wtmp.function('N1_ratio_%s'%POI[2]),wtmp.function(POI[2]+str(par_max[POI[2]])))
 		N1_3d		= RooFormulaVar('N1_3d','N1_3d','1/(1+@0*@1+@2*@3+@4*@5)',paralist)
 		N2_3d		= RooFormulaVar('N2_3d','N2_3d','(@0*@1)/(1+@0*@1+@2*@3+@4*@5)',paralist)
 		N3_3d		= RooFormulaVar('N3_3d','N3_3d','(@2*@3)/(1+@0*@1+@2*@3+@4*@5)',paralist)	
 		N4_3d		= RooFormulaVar('N4_3d','N4_3d','(@4*@5)/(1+@0*@1+@2*@3+@4*@5)',paralist)	
-		model		= RooAddPdf('aTGC_model','aTGC_model',RooArgList(SMPdf,wtmp.pdf('Pdf_%s'%POI[0]),wtmp.pdf('Pdf_%s'%POI[1]),wtmp.pdf('Pdf_%s'%POI[2])),RooArgList(N1_3d,N2_3d,N3_3d,N4_3d))
-		normfactor_3d	= RooFormulaVar('normfactor_3d','normfactor_3d','1+(@0-1)+(@1-1)+(@2-1)',RooArgList(wtmp.function('scaleshape_%s'%POI[0]),wtmp.function('scaleshape_%s'%POI[1]),wtmp.function('scaleshape_%s'%POI[2])))
+		model		= RooAddPdf('aTGC_model','aTGC_model',\
+					    RooArgList(SMPdf,wtmp.pdf('Pdf_%s'%POI[0]),wtmp.pdf('Pdf_%s'%POI[1]),wtmp.pdf('Pdf_%s'%POI[2])),\
+					    RooArgList(N1_3d,N2_3d,N3_3d,N4_3d))
+		normfactor_3d	= RooFormulaVar('normfactor_3d_%s'%ch,'normfactor_3d_%s'%ch,'1+(@0-1)+(@1-1)+(@2-1)',\
+						RooArgList(wtmp.function('scaleshape_%s_%s'%(POI[0],ch)),\
+							   wtmp.function('scaleshape_%s_%s'%(POI[1],ch)),\
+							   wtmp.function('scaleshape_%s_%s'%(POI[2],ch))))
 		getattr(WS,'import')(normfactor_3d)	
 		getattr(wtmp,'import')(normfactor_3d)	
 		model.Print()
@@ -291,7 +301,7 @@ def make_input(ch = 'ele'):
 		
 		
 		if options.do_plots:
-			make_plots(rrv_mass_lvj,wtmp)
+			make_plots(rrv_mass_lvj,wtmp,ch)
 			#cross check
 			canvas		= TCanvas(POI[0]+','+POI[1]+','+POI[2],POI[0]+','+POI[1]+','+POI[2],1)
 			p4		= rrv_mass_lvj.frame()
@@ -305,14 +315,14 @@ def make_input(ch = 'ele'):
 			datahist_all3.plotOn(p4,RooFit.MarkerColor(kRed))
 			for i in range(3):
 					wtmp.var(POI[i]).setVal(0)
-			model.plotOn(p4,RooFit.LineColor(kBlue),RooFit.Normalization(wtmp.function('normfactor_3d').getVal()*wtmp.data('SMdatahist').sumEntries(),RooAbsReal.NumEvent))
+			model.plotOn(p4,RooFit.LineColor(kBlue),RooFit.Normalization(wtmp.function('normfactor_3d_%s'%ch).getVal()*wtmp.data('SMdatahist').sumEntries(),RooAbsReal.NumEvent))
 			for i in range(3):
 					wtmp.var(POI[i]).setVal(par_max[POI[i]])
-			model.plotOn(p4,RooFit.LineColor(kRed),RooFit.Normalization(wtmp.function('normfactor_3d').getVal()*wtmp.data('SMdatahist').sumEntries(),RooAbsReal.NumEvent))
+			model.plotOn(p4,RooFit.LineColor(kRed),RooFit.Normalization(wtmp.function('normfactor_3d_%s'%ch).getVal()*wtmp.data('SMdatahist').sumEntries(),RooAbsReal.NumEvent))
 			for j in range(14):
 				for i in range(3):
 					wtmp.var(POI[i]).setVal(par_max[POI[i]]/15.0 * (j+1))
-				normval = wtmp.function('normfactor_3d').getVal()*wtmp.data('SMdatahist').sumEntries()
+				normval = wtmp.function('normfactor_3d_%s'%ch).getVal()*wtmp.data('SMdatahist').sumEntries()
 				model.plotOn(p4,RooFit.LineColor(kGreen),RooFit.LineStyle(kDashed),RooFit.Normalization(normval,RooAbsReal.NumEvent))
 			wtmp.data('SMdatahist').plotOn(p4,RooFit.MarkerColor(kBlack))
 			datahist_all3.plotOn(p4,RooFit.MarkerColor(kRed))
@@ -322,8 +332,7 @@ def make_input(ch = 'ele'):
 			p4.Draw()
 			canvas.SetLogy
 			canvas.Update()
-			raw_input('...')
-
+			raw_input('cross check plot plotted')
 
 	#import to workspace
 	#getattr(w,'import')(model)
