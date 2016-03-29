d7d9f5c959fb4aa88882be75c91d7c76e2bec2ae
Christoph Renner <undeo@student.kit.edu>
2016-03-29 16:36:59 +0200

    small updates

--- CombinedEWKAnalysis/CommonTools/test/create_workspace4combine.py
+++ CombinedEWKAnalysis/CommonTools/test/create_workspace4combine.py
@@ -15,7 +15,7 @@ if options.newtrees:
 if options.do_plots:
 	signalmodelcommand += '-p '
 
-datacard	= 'aC_WWWZ.txt'
+datacardname	= 'aC_WWWZ.txt'
 model 		= 'CombinedEWKAnalysis.CommonTools.ACModel:par1par2par3_TF3_Model'
 channels	= 'ch_WW_ele,ch_WW_mu,ch_WZ_ele,ch_WZ_mu'
 wsout		= 'workspace_WWWZ.root'
@@ -24,17 +24,25 @@ r_ccw		= 'range_ccw=-30,30'
 r_cb		= 'range_cb=-70,70'
 pois		= 'poi=cwww,ccw,cb'
 
-print '### creating singal model WW ###'
+print '''################################
+### creating singal model WW ###
+################################'''
 print signalmodelcommand + '--cat WW'
 os.system(signalmodelcommand + '--cat WW')
-print '### creating singal model WZ ###'
+print '''################################
+### creating singal model WZ ###
+################################'''
 print signalmodelcommand + '--cat WZ'
 os.system(signalmodelcommand + '--cat WZ')
 
-print '### creating workspace for text2workspace.py ###'
-print 'python buildWorkspace_WWWZ.py --config=config_ATGC2 -o %s'%datacard
-os.system('python buildWorkspace_WWWZ.py --config=config_ATGC2 -o %s'%datacard)
+print '''################################################
+### creating workspace for text2workspace.py ###
+################################################'''
+print 'python buildWorkspace_WWWZ.py --config=config_ATGC2 -o %s'%datacardname
+os.system('python buildWorkspace_WWWZ.py --config=config_ATGC2 -o %s'%datacardname)
 
-print '### creating final workspace ###'
-print 'text2workspace.py %s -o %s -P %s --PO channels=%s --PO %s --PO %s --PO %s --PO %s'%(datacard,wsout,model,channels,pois,r_cwww,r_ccw,r_cb)
-os.system('text2workspace.py %s -o %s -P %s --PO channels=%s --PO %s --PO %s --PO %s --PO %s'%(datacard,wsout,model,channels,pois,r_cwww,r_ccw,r_cb))
+print '''################################
+### creating final workspace ###
+################################'''
+print 'text2workspace.py %s -o %s -P %s --PO channels=%s --PO %s --PO %s --PO %s --PO %s'%(datacardname,wsout,model,channels,pois,r_cwww,r_ccw,r_cb)
+os.system('text2workspace.py %s -o %s -P %s --PO channels=%s --PO %s --PO %s --PO %s --PO %s'%(datacardname,wsout,model,channels,pois,r_cwww,r_ccw,r_cb))
