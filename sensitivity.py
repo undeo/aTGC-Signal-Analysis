b03126f523d662deaf49835c468e6b68d598c066
Christoph Renner <undeo@student.kit.edu>
2016-07-15 13:01:07 +0200

    now easier to use

--- sensitivity.py
+++ sensitivity.py
@@ -3,80 +3,54 @@ import os
 from optparse import OptionParser
 
 parser	= OptionParser()
-parser.add_option('-P', dest='POI', default='cwww')
+parser.add_option('-P', '--POI', dest='POI', default='cwww')
+parser.add_option('--pos', action='store_true', default=False)
+parser.add_option('--neg', action='store_true', default=False)
 (options,args) = parser.parse_args()
 
-startvalcwww = 10.6
-endvalcwww = 10.7
-intvalcwww = 0.01
-iterscwww = abs(int((endvalcwww-startvalcwww)/intvalcwww))
-startvalccw = 13.68
-endvalccw = 13.8
-intvalccw = 0.01
-itersccw = abs(int((endvalccw-startvalccw)/intvalccw))
-startvalcb = 60.
-endvalcb = 63
-intvalcb = 0.1
-iterscb = abs(int((endvalcb-startvalcb)/intvalcb))
 
-cwww=False
-ccw=False
-cb=False
-if options.POI=='cwww':
-	cwww=True
-elif options.POI=='ccw':
-	ccw=True
-elif options.POI=='cb':
-	cb=True
+if options.neg:
+	startvals	= {'cwww' : -8.28, 'ccw' : -11.4, 'cb' : -53}
+	endvals		= {'cwww' : -8.58, 'ccw' : -11.8, 'cb' : -60}
+	intvals		= {'cwww' : -0.01, 'ccw' : -0.01, 'cb' : -0.1}
+elif options.pos:
+	startvals	= {'cwww' : 8.4, 'ccw' : 10.5, 'cb' : 51.5}
+	endvals		= {'cwww' : 8.9, 'ccw' : 10.9, 'cb' : 62.5}
+	intvals		= {'cwww' : 0.01, 'ccw' : 0.01, 'cb' : 0.1}
+else:
+	raise RuntimeError('run with either --pos or --neg!')
+iters	= {'cwww' : abs(int((endvals['cwww']-startvals['cwww'])/intvals['cwww'])), 'ccw' : abs(int((endvals['ccw']-startvals['ccw'])/intvals['ccw'])), 'cb' : abs(int((endvals['cb']-startvals['cb'])/intvals['cb']))}
+
+
+POI	= options.POI 
+if POI=='cwww':
+	noPOI	= ['ccw','cb']
+	vals	= {'cwww' : startvals[POI], 'ccw' : 0,'cb' : 0}
+elif POI=='ccw':
+	noPOI	= ['cwww','cb']
+	vals	= {'cwww' : 0,'ccw' : startvals[POI], 'cb' : 0}
+elif POI=='cb':
+	noPOI	= ['cwww','ccw']
+	vals	= {'cwww': 0,'ccw' : 0, 'cb' : startvals[POI]}
+
+
 
 #cwww
-if cwww:
-	for i in range(iterscwww):
-		comstring = 'combine workspace_WWWZ_linter1.root -M MultiDimFit --floatOtherPOIs=0 --algo grid -t -1 --expectSignal=1 --points 3 --freezeNuisances ccw,cb --setPhysicsModelParameters cwww=%s,ccw=0,cb=0 -n _senscwww -P cwww --setPhysicsModelParameterRange cwww=-2,2'%(startvalcwww+intvalcwww*i)
-		print comstring
-		os.system(comstring)
+for i in range(iters[POI]):
+	vals[POI] = startvals[POI] + intvals[POI]*i
+	comstring = 'combine workspace_WWWZ_linter1.root -M MultiDimFit --floatOtherPOIs=0 --algo grid -t -1 --expectSignal=1 --points 3 --freezeNuisances %s,%s --setPhysicsModelParameters cwww=%s,ccw=%s,cb=%s -n _sens%s -P %s --setPhysicsModelParameterRange %s=-2,2'%(noPOI[0],noPOI[1],vals['cwww'],vals['ccw'],vals['cb'],POI,POI,POI)
+	print comstring
+	os.system(comstring)
 
-		fileIn = TFile.Open('higgsCombine_senscwww.MultiDimFit.mH120.root')
-		tree = fileIn.Get('limit')
-		for j in range(3):
-			tree.GetEntry(1+j)
-			if 2*tree.deltaNLL>3.84:
-				print "stop, %s - %s"%(startvalcwww+intvalcwww*(i-1),startvalcwww+intvalcwww*i)
-				fileIn.Close()
-				exit(0)
-		fileIn.Close()
+	fileIn = TFile.Open('higgsCombine_sens%s.MultiDimFit.mH120.root'%POI)
+	tree = fileIn.Get('limit')
+	for j in range(3):
+		tree.GetEntry(1+j)
+		if 2*tree.deltaNLL>3.84:
+			print "stop, %s - %s"%(startvals[POI]+intvals[POI]*(i-1),startvals[POI]+intvals[POI]*i)
+			fileIn.Close()
+			exit(0)
+	fileIn.Close()
 
 
-#ccw
-if ccw:	
-	for i in range(itersccw):
-		comstring = 'combine workspace_WWWZ_linter2.root -M MultiDimFit --floatOtherPOIs=0 --algo grid -t -1 --expectSignal=1 --points 3 --freezeNuisances cwww,cb --setPhysicsModelParameters cwww=0,ccw=%s,cb=0 -n _sensccw -P ccw --setPhysicsModelParameterRange ccw=-2,2'%(startvalccw+intvalccw*i)
-		print comstring
-		os.system(comstring)
 
-		fileIn = TFile.Open('higgsCombine_sensccw.MultiDimFit.mH120.root')
-		tree = fileIn.Get('limit')
-		for j in range(3):
-			tree.GetEntry(1+j)
-			if 2*tree.deltaNLL>3.84:
-				print "stop, %s - %s"%(startvalccw+intvalccw*(i-1),startvalccw+intvalccw*i)
-				fileIn.Close()
-				exit(0)
-		fileIn.Close()
-
-#cb
-if cb:	
-	for i in range(iterscb):
-		comstring = 'combine workspace_WWWZ_linter3.root -M MultiDimFit --floatOtherPOIs=0 --algo grid -t -1 --expectSignal=1 --points 3 --freezeNuisances cwww,ccw --setPhysicsModelParameters cwww=0,ccw=0,cb=%s -n _senscb -P cb --setPhysicsModelParameterRange cb=-2,2'%(startvalcb+intvalcb*i)
-		print comstring
-		os.system(comstring)
-
-		fileIn = TFile.Open('higgsCombine_senscb.MultiDimFit.mH120.root')
-		tree = fileIn.Get('limit')
-		for j in range(3):
-			tree.GetEntry(1+j)
-			if 2*tree.deltaNLL>3.84:
-				print "stop, %s - %s"%(startvalcb+intvalcb*(i-1),startvalcb+intvalcb*i)
-				fileIn.Close()
-				exit(0)
-		fileIn.Close()
